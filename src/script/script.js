375aa84bfbeaf2a4d603e357371d50bf42743743
amdymstfa <amdymoustapha011@gmail.com>
2024-11-02 12:40:36 +0100

    done : review html css tailwind

--- src/script/script.js
+++ src/script/script.js
@@ -1,3 +1,4 @@
+// this function is calling when the buttun for adding task is clicked
 function AddCard(){
     const addCard = document.getElementById('add-card') ;
     addCard.classList.toggle('hidden') ;
